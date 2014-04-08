8c8317ed012effbc2f7f543fd5acf79c618fb300
coltonmorris <coltonmorris94@gmail.com>
2014-04-07 22:06:40 -0600

    hello git world

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'unirest-php/lib/Unirest/Unirest.php';
+require_once 'unirest-php/lib/Unirest.php';
 $ary =  array("X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM");
 $response = Unirest::get("https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAggregatedStats/1324523",$ary,null);
 echo "hello world";
