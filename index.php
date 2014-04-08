941b11b2ea1d0ddaed4a753604ca2620b4d61ccc
coltonmorris <coltonmorris94@gmail.com>
2014-04-07 21:58:55 -0600

    hello git world

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-//$response = Unirest::get(
-//  "https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAggregatedStats/1324523",
-//  array(
-//    "X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM"
-//  ),
-//  null
-//);
+require_once 'unirest-php/lib/Unirest/Unirest.php';
+$response = Unirest::get(
+  "https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAggregatedStats/1324523",
+  array(
+    "X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM"
+  ),
+  null
+);
 echo "hello world";
 
 ?>
 <HTML>
 <head>
-
 </head>
 <body>
 Colton's Website!
-<a href="test.php">test</a>
 </body>
 </HTML>
