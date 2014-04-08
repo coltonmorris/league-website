73d1423685a3276cbb0b92bdcca836ad5602f709
coltonmorris <coltonmorris94@gmail.com>
2014-04-07 22:18:21 -0600

    hello git world

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 require_once 'unirest-php/lib/Unirest.php';
-$ary =  array("X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM");
-$summonerID = '38918850';
+function getLeagueForPlayer($summonerID,$region){
+	$ary =  array("X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM");
+	$response = Unirest::get("https://community-league-of-legends.p.mashape.com/api/v1.0/$region/summoner/getLeagueForPlayer/$summonerID",$ary,null);
+	return $response;
+}
+function getSummonerByName($summonerName,$region){
+	$ary =  array("X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM");
+	$response = Unirest::get("https://community-league-of-legends.p.mashape.com/api/v1.0/$region/summoner/getLeagueForPlayer/$summonerName",$ary,null);
+	return $response;
+}
+
 $region = 'NA';
-$response = Unirest::get("https://community-league-of-legends.p.mashape.com/api/v1.0/$region/summoner/getLeagueForPlayer/$summonerID",$ary,null);
+$summonerName = 'kingrazy';
+$response = getSummonerByName($summonerName,$region);
 
 echo "<PRE>";
 print_r($response);
 echo "hello world";
 echo "</PRE>";
+
+$summonerID = '38918850';
+getLeagueForPlayer($summonerID,$region);
 ?>
 <HTML>
 <head>
