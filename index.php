985c23345d0a09833c574f6336016d1d17e06afc
coltonmorris <coltonmorris94@gmail.com>
2014-04-07 21:51:31 -0600

    hello git world

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<?php
+$response = Unirest::get(
+  "https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAggregatedStats/1324523",
+  array(
+    "X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM"
+  ),
+  null
+);
+echo "hello world";
+
+?>
 <HTML>
 <head>
+
 </head>
 <body>
 Colton's Website!
