d4b0c880432de760ab03e3c72bd457c0f283220f
coltonmorris <coltonmorris94@gmail.com>
2014-04-08 22:44:20 -0600

    hello git world

--- index.php
+++ index.php
@@ -3,19 +3,14 @@ require_once 'unirest-php/lib/Unirest.php';
 function getLeagueForPlayer($summonerID,$region){
 	$ary =  array("X-Mashape-Authorization" => "fq3PY66RHuHbiQxQ5UamofWTBGcAV2IM");
 	$response = Unirest::get("https://community-league-of-legends.p.mashape.com/api/v1.0/$region/summoner/getLeagueForPlayer/$summonerID",$ary,null);
-	//return $response;
 	//$json = file_get_contents($url);
-echo "<PRE>";
-print_r($response);
-echo "</PRE>";
-echo "<br />";
-echo "<br />";
-echo "<br />";
-echo "<br />";
+	echo "<PRE>";
+	print_r($response);
+	echo "</PRE>";
 	$obj = json_decode($response);
-echo "<PRE>";
-print_r($obj);
-echo "</PRE>";
+	echo "<PRE>";
+	print_r($obj);
+	echo "</PRE>";
 	//return $obj->resultItemList;
 	return $obj;
 }
@@ -31,7 +26,7 @@ $response = getSummonerByName($summonerName,$region);
 
 
 $summonerID = '38918850';
-$response = getLeagueForPlayer($summonerID,$region);
+//$response = getLeagueForPlayer($summonerID,$region);
 
 ?>
 <HTML>
